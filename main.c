442d6c6444e187b8ba6fa3144f5eae6cb339685d
jasonjiangstuy <jasonjiangny@gmail.com>
2021-10-26 15:11:04 -0400

    finished hw

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "random.h"
+#include <unistd.h>
 
 int main() {
   int i ;
@@ -6,6 +7,17 @@ int main() {
   int* randomData = generate_random(10);
   for (i = 0; i < 10; i ++){
     printf("%d\n", randomData[i]);
+
+  }
+
+  int fh = open("./test.txt", O_RDWR, 0777);
+  write( fh, randomData, 10 * sizeof(int));
+  int* newArray = calloc(10, sizeof(int));
+  fh = open("./test.txt", O_RDWR, 0777);
+  read(fh, newArray, 10 * sizeof(int));
+  printf("new ARRAY\n");
+  for (i = 0; i < 10; i ++){
+    printf("%d\n", newArray[i]);
 
   }
   // printf("%d\n", generate_random(50));
